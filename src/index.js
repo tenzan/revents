bcc807c5eb432e888e2d8c6129325876b8340668
Askar Karasaev <askar.karasaev@gmail.com>
2019-11-25 15:31:57 +0900

    Folder structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 
 const rootEl = document.getElementById('root');
@@ -11,7 +11,7 @@ let render = () => {
 }
 
 if (module.hot) {
-    module.hot.accept('./App', () => {
+    module.hot.accept('./app/layout/App', () => {
         setTimeout(render);
     })
 }
